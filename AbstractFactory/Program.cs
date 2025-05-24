b37bd445d8ae73a393d13e6a43e1c322a15c4d17
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2025-05-24 18:48:33 -0300

    Terminado o programa do AbstractFactory
    Seleção de personagens e construção automática

--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -1,1 +1,20 @@
 ﻿using AbstractFactory.Models;
+
+IFabricaBases fabrica;
+Console.WriteLine("Escolha sua base para a luta: vitor, elson, miguel");
+
+switch (Console.ReadLine())
+{
+    case "vitor":
+        fabrica = new FabricaBasesVitor();
+        break;
+    case "elson":
+        fabrica = new FabricaBasesElson();
+        break;
+    case "miguel":
+        fabrica = new FabricaBasesMiguel();
+        break;
+    default:
+        Console.WriteLine("esse personagem não esta disponivel");
+        break;
+}
