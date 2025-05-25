49636b09614568b7df26dc40ae11d4002701a99c
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2025-05-25 00:49:32 -0300

    Criando os clones do Vitor, finalizando o Prototype
    Criando e testando os clones criados pelo gerenciador de vitors

--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Prototype.Models;
+
+GerenciadorDeVitor gerenciadorDeVitor = new GerenciadorDeVitor();
+
+gerenciadorDeVitor["padrão"] = new VitorConcreto("corpo de vitor", "mente de vitor");
+gerenciadorDeVitor["personalizada"] = new VitorConcreto("corpo de vitor saudavel", "mente de vitor triste");
+
+VitorConcreto clone = gerenciadorDeVitor["padrão"].clone() as VitorConcreto;
+VitorConcreto clone2 = gerenciadorDeVitor["personalizada"].clone() as VitorConcreto;
--- Prototype/obj/Debug/net8.0/Prototype.AssemblyInfo.cs
+++ Prototype/obj/Debug/net8.0/Prototype.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prototype")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0195457c466f7039654c23d96ee906cc38b65f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b777445e2410847d7fbc305cfa7b936dfee10e59")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prototype")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prototype")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
