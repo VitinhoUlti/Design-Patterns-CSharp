9e43ac0abe7855a1014236adcc581030e5296d2f
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2025-05-24 23:53:50 -0300

    Personagem Elson sendo criado e fim do builder

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -8,3 +8,8 @@ criadorDePersonagem = new CriadorDeVitor();
 criador.ConstruirPersonagem(criadorDePersonagem);
 personagem = criadorDePersonagem.ObterPersonagem();
 Console.WriteLine($"Vitor entra na siulação, ele tem {personagem.Cabeça}, {personagem.Torso} e {personagem.Perna}");
+
+criadorDePersonagem = new CriadorDeElson();
+criador.ConstruirPersonagem(criadorDePersonagem);
+personagem = criadorDePersonagem.ObterPersonagem();
+Console.WriteLine($"Elson entra na siulação, ele tem {personagem.Cabeça}, {personagem.Torso} e {personagem.Perna}");
