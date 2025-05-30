09d7fb75ee4cf0a465fdfa4d7b9b4e0258f33b59
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2025-05-30 16:08:37 -0300

    Teste da adaptação das classes
    Teste dos métodos das classes e finalização do Adapter

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using Adapter.Models;
+
+Tomada2Pinos tomada2Pinos = new Tomada2Pinos();
+Cabo3Pinos cabo3Pinos = new Cabo3Pinos();
+
+Adaptador adaptador = new Adaptador(cabo3Pinos);
+
+Console.WriteLine("Colocou o cabo na tomada de 2 pinos");
+tomada2Pinos.ReceberEnergia();
+tomada2Pinos.Energizar();
+
+Console.WriteLine("Pegou um adaptador e inseriu na tomada de 3 pinos");
+cabo3Pinos.ReceberEnergia3Pinos();
+cabo3Pinos.EnergizarMaisPotente();
